82ad7e52c0bb18c84db348ff9ba19cd624ad03c4
KzZe-Sama <lilmisstreebox@gmail.com>
2020-06-12 10:05:59 +0545

    commits only to master

---
+++ js/javascript.js
@@ -0,0 +1,51 @@
+
+    
+    function toggle1() {
+				var p1 = document.getElementById("1"); //1st image
+				 
+				
+				if (p1.style.display == "none") // if 1st image is hidden
+				    { 
+					p1.style.display = "block";   // display hidden image
+					
+				} 
+				else {							  
+					
+				}
+			}
+			document.getElementById("1").style.display = "none"; // by default show 1st image
+		
+		
+		
+    function toggle2() {
+				var p2 = document.getElementById("2"); //1st image
+				 
+				
+				if (p2.style.display == "none") // if 1st image is hidden
+				    { 
+					p2.style.display = "block";   // display hidden image
+					
+				} 
+				else {							  
+					
+				}
+			}
+			document.getElementById("2").style.display = "none"; // by default show 1st image
+		
+		
+		
+    function toggle3() {
+				var p3 = document.getElementById("3"); //1st image
+				 
+				
+				if (p3.style.display == "none") // if 1st image is hidden
+				    { 
+					p3.style.display = "block";   // display hidden image
+					
+				} 
+				else {							  
+					
+				}
+			}
+			document.getElementById("3").style.display = "none"; // by default show 1st image
+		
